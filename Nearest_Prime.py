708ef41d3af7a1dc592a2ccf4fc5ecf97e70d7da
Kusumagali <97952344+Kusumagali@users.noreply.github.com>
2023-08-12 11:38:05 +0530

    def fun(n):     if n<2:         return False     for i in range(2,n):         if n%i == 0:             return False     return True
    for _ in range(int(input())):
        n = int(input())
        a = n-1
        b = n+1
        while 1:
            if fun(a) and fun(b):
                print(min(a,b))
                break
            elif fun(a):
                print(a)
                break
            elif fun(b):
                print(b)
                break
            elif fun(n):
                print(n)
                break
            a-=1
            b+=1

---
+++ Nearest_Prime.py
@@ -0,0 +1,27 @@
+def fun(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
+
+for _ in range(int(input())):
+    n = int(input())
+    a = n-1
+    b = n+1
+    while 1:
+        if fun(a) and fun(b):
+            print(min(a,b))
+            break
+        elif fun(a):
+            print(a)
+            break
+        elif fun(b):
+            print(b)
+            break
+        elif fun(n):
+            print(n)
+            break
+        a-=1
+        b+=1
