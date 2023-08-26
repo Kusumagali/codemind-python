093ed93624acb20afcf1315e11aa5d000619e5a1
Kusumagali <97952344+Kusumagali@users.noreply.github.com>
2023-08-27 01:12:12 +0530

    n = int(input()) arr = list(map(int,input().split())) k = int(input()) def fun(arr,k):     return k in arr print(fun(arr,k))

--- Element_search_in_an_array.py
+++ Element_search_in_an_array.py
@@ -1,7 +1,6 @@
 n = int(input())
 arr = list(map(int,input().split()))
 k = int(input())
-if k in arr:
-    print('True')
-else:
-    print("False")
+def fun(arr,k):
+    return k in arr
+print(fun(arr,k))
